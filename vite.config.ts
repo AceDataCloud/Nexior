be124de6bbcd88b0e2aed260f37e234c3f02813c
Germey <cqc@cuiqingcai.com>
2021-08-28 00:17:39 +0800

    update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,22 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import * as path from 'path';
 
 export default defineConfig({
-  plugins: [vue()]
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8000',
+        changeOrigin: true
+      }
+    }
+  },
+  plugins: [
+    vue()
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  }
 });
