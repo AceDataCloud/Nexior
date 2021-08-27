159ed678f8c779944b9f5974c5583bd098dc6dee
Germey <cqc@cuiqingcai.com>
2021-08-28 00:52:39 +0800

    add i18n

---
+++ src/i18n/en/index.ts
@@ -0,0 +1,16 @@
+export default {
+  message: {
+    copied: 'Copied'
+  },
+  menu: {
+    home: 'Home',
+    editor: 'Editor'
+  },
+  option: {
+    theme: 'Theme',
+    highlight: 'Code Highlight Theme',
+    bold: 'Bold',
+    italic: 'Italic',
+    strikeout: 'Strikeout'
+  }
+};
---
+++ src/i18n/zh/index.ts
@@ -0,0 +1,16 @@
+export default {
+  message: {
+    copied: '复制成功'
+  },
+  menu: {
+    home: '首页',
+    editor: '编辑器'
+  },
+  option: {
+    theme: '主题',
+    highlight: '代码高亮',
+    bold: '粗体',
+    italic: '斜体',
+    strikeout: '删除线'
+  }
+};
