c9de05eb976642756aa5cec7666ea39ae1dc4412
Germey <cqc@cuiqingcai.com>
2021-08-28 01:46:04 +0800

    add layouts

---
+++ src/layouts/index.ts
@@ -0,0 +1,4 @@
+import LayoutHome from '@/layouts/Home.vue';
+import LayoutEditor from '@/layouts/Editor.vue';
+
+export { LayoutHome, LayoutEditor };
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import HelloWorld from '@/components/HelloWorld.vue';
 
 const routes = [
   {
     path: '/',
-    name: 'HelloWorld',
-    component: HelloWorld
+    name: 'Home',
+    component: () => import('@/pages/Home.vue')
   }
 ];
 
