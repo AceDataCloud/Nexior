504097d4078f89631114f66789e07231edb050d3
AceDataCloud <office@acedata.cloud>
2021-08-28 00:28:15 +0800

    add vuex

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,6 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import './style.css';
 import router from './router/index';
+import store from './store/index';
 
-createApp(App).use(router).mount('#app');
+createApp(App).use(router).use(store).mount('#app');
---
+++ src/store/index.ts
@@ -0,0 +1,21 @@
+import { createApp } from 'vue';
+import { createStore } from 'vuex';
+
+interface IState {
+  count: number;
+}
+
+const store = createStore({
+  state(): IState {
+    return {
+      count: 0
+    };
+  },
+  mutations: {
+    increment(state: IState) {
+      state.count++;
+    }
+  }
+});
+
+export default store;
