69fdb3a933b92fcf0db0d2f30e6766a0dfeadbd0
AceDataCloud <office@acedata.cloud>
2021-08-28 00:52:39 +0800

    add i18n

---
+++ src/i18n/index.ts
@@ -0,0 +1,22 @@
+import enLang from './en/index';
+import zhLang from './zh/index';
+import enLocale from 'element-plus/lib/locale/lang/en';
+import zhLocale from 'element-plus/lib/locale/lang/zh-cn';
+import { createI18n } from 'vue-i18n';
+
+const i18n = createI18n({
+  locale: zhLocale.name,
+  fallbackLocale: enLocale.name,
+  messages: {
+    [enLocale.name]: {
+      el: enLocale.el,
+      ...enLang
+    },
+    [zhLocale.name]: {
+      el: zhLocale.el,
+      ...zhLang
+    }
+  }
+});
+
+export default i18n;
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
 import { createApp } from 'vue';
 import App from './App.vue';
-import './style.css';
 import router from './router/index';
 import store from './store/index';
 import ElementPlus from 'element-plus';
+import i18n from './i18n/index';
+import './style.css';
 import 'element-plus/dist/index.css';
 
-createApp(App).use(router).use(store).use(ElementPlus).mount('#app');
+createApp(App).use(router).use(store).use(ElementPlus).use(i18n).mount('#app');
