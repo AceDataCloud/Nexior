6174e0a41a193fcad944d4e88baacda5cfc1253d
Germey <cqc@cuiqingcai.com>
2021-08-28 00:46:42 +0800

    add element plus

--- src/main.ts
+++ src/main.ts
@@ -3,5 +3,7 @@ import App from './App.vue';
 import './style.css';
 import router from './router/index';
 import store from './store/index';
+import ElementPlus from 'element-plus';
+import 'element-plus/dist/index.css';
 
-createApp(App).use(router).use(store).mount('#app');
+createApp(App).use(router).use(store).use(ElementPlus).mount('#app');
