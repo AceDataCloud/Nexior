2c33cfacee95663dbf401ca0a7a9455cb17e91a3
Germey <cqc@cuiqingcai.com>
2021-08-27 23:59:25 +0800

    add tailwind

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref } from 'vue';
 
-defineProps<{ msg: string }>()
+defineProps<{ msg: string }>();
 
-const count = ref(0)
+const count = ref(0);
 </script>
 
 <template>
@@ -19,15 +19,13 @@ const count = ref(0)
   <p>See <code>README.md</code> for more information.</p>
 
   <p>
-    <a href="https://vitejs.dev/guide/features.html" target="_blank">
-      Vite Docs
-    </a>
+    <a href="https://vitejs.dev/guide/features.html" target="_blank"> Vite Docs </a>
     |
     <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Docs</a>
   </p>
 
   <button type="button" @click="count++">count is: {{ count }}</button>
-  <p>
+  <p class="mt-7">
     Edit
     <code>components/HelloWorld.vue</code> to test hot module replacement.
   </p>
