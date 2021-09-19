74f62354a2999717ba22e4852fe21471e28ac552
AceDataCloud <office@acedata.cloud>
2021-09-19 22:18:26 +0800

    add dayjs plugin

--- src/main.ts
+++ src/main.ts
@@ -6,5 +6,15 @@ import ElementPlus from 'element-plus';
 import i18n from './i18n/index';
 import './assets/css/tailwind.css';
 import './assets/scss/style.scss';
+import dayjs from './plugins/dayjs';
 
-createApp(App).use(router).use(store).use(ElementPlus).use(i18n).mount('#app');
+const app = createApp(App);
+
+app.use(router);
+app.use(store);
+app.use(ElementPlus);
+app.use(i18n);
+app.use(dayjs, {
+  formatString: 'YYYY-MM-DD HH:mm:ss'
+});
+app.mount('#app');
--- src/settings/article.ts
+++ src/settings/article.ts
@@ -1,1 +1,2 @@
 export const DEFAULT_LIMIT = 10;
+export const DATE_FORMAT = 'yyyy-MM-dd HH:mm:ss';
