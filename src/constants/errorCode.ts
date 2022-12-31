866331343c7cc85e8058b92aeea45abdd7edfa41
AceDataCloud <office@acedata.cloud>
2023-01-01 00:38:03 +0800

    add apply operation

--- src/constants/errorCode.ts
+++ src/constants/errorCode.ts
@@ -1,1 +1,2 @@
 export const ERROR_CODE_UNVERIFIED = 'unverifed';
+export const ERROR_CODE_DUPLICATION = 'duplication';
---
+++ src/i18n/zh/application/message.ts
@@ -0,0 +1,4 @@
+export default {
+  alreadyApplied: '已经申请过该服务，请到控制台查看',
+  applySuccessfully: '申请成功，即将跳转到控制台'
+};
