c7fdcd0a4b53398e3133b6bdc093480fd0463cba
AceDataCloud <office@acedata.cloud>
2023-01-22 13:02:46 +0800

    markdown render

---
+++ src/components/common/MarkdownRenderer.vue
@@ -0,0 +1,27 @@
+<template>
+  <vue-markdown :source="content" class="markdown-body" />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import VueMarkdown from 'vue-markdown-render';
+
+export default defineComponent({
+  name: 'ApiCode',
+  components: {
+    VueMarkdown
+  },
+  props: {
+    content: {
+      type: String,
+      required: true
+    }
+  },
+  data() {},
+  watch: {}
+});
+</script>
+
+<style lang="scss">
+@import 'github-markdown-css/github-markdown.css';
+</style>
