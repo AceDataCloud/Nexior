537ef21cf3b3f5b870b97beb7d37cfeb972bb562
AceDataCloud <office@acedata.cloud>
2023-01-25 23:35:37 +0800

    fix env

---
+++ src/constants/env.ts
@@ -0,0 +1,3 @@
+export const ENV_LOCAL = 'local';
+export const ENV_TEST = 'test';
+export const ENV_PRODUCTION = 'production';
--- src/utils/env.ts
+++ src/utils/env.ts
@@ -1,7 +1,19 @@
+import { ENV_LOCAL, ENV_PRODUCTION, ENV_TEST } from '@/constants';
+
+const PATTERN_TEST_ENV = '.test.zhishuyun.com';
+const PATTERN_LOCAL_ENV = '.lcoal.zhishuyun.com';
+
 /**
  * Get env of App, return local, test, production
  * @returns
  */
 export const getEnv = () => {
-  return import.meta.env.VITE_APP_ENV;
+  const host = window.location.host;
+  if (host.indexOf(PATTERN_LOCAL_ENV) > 0) {
+    return ENV_LOCAL;
+  }
+  if (host.indexOf(PATTERN_TEST_ENV) > 0) {
+    return ENV_TEST;
+  }
+  return ENV_PRODUCTION;
 };
