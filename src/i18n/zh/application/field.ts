be58529eefa9a85813d463f18b718ab15be0b50a
Germey <cqc@cuiqingcai.com>
2023-02-25 16:04:00 +0800

    add console filter types

--- src/i18n/zh/application/field.ts
+++ src/i18n/zh/application/field.ts
@@ -1,11 +1,13 @@
 export default {
-  remainingCount: '剩余次数',
-  usedCount: '已使用次数',
+  remainingCount: '剩余量',
+  usedCount: '使用量',
   apiKey: 'API Key',
   service: '服务',
   operation: '操作',
   amount: '数量',
   shouldPayPrice: '应付金额',
   api: 'API',
-  credential: '密钥'
+  proxy: '代理',
+  credential: '密钥',
+  name: '名称'
 };
