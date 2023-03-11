c05b458b69bfcbb17d6a3fe760bf44cfd0fea113
Germey <cqc@cuiqingcai.com>
2023-03-11 10:00:10 +0800

    update price plan

--- src/i18n/zh/api/message.ts
+++ src/i18n/zh/api/message.ts
@@ -1,4 +1,8 @@
 export default {
   free: '免费',
-  appliedCount: '申请次数'
+  appliedCount: '申请次数',
+  firstFree: '首次送',
+  custom: '指定次数',
+  present: '赠送',
+  around: '约'
 };
--- src/i18n/zh/service/message.ts
+++ src/i18n/zh/service/message.ts
@@ -1,7 +1,7 @@
 export default {
   free: '免费',
   appliedCount: '申请次数',
-  indexTitle: '方便易用的数字化解决方案',
+  indexTitle: '数字化服务的智慧之选',
   indexSubtitle: '用一个个标准化服务，助力你工作与生活的方方面面。',
   fromPrice: '起'
 };
