3d8a5d6f410f6ea2eaa23851c3ec6728d7aaac3f
Germey <cqc@cuiqingcai.com>
2023-03-19 21:35:07 +0800

    update apply success zh

--- src/i18n/zh/application/message.ts
+++ src/i18n/zh/application/message.ts
@@ -1,6 +1,6 @@
 export default {
   alreadyApplied: '已经申请过该服务，请到控制台查看',
-  applySuccessfully: '申请成功，即将跳转到控制台',
+  applySuccessfully: '申请成功',
   notApplied: '你尚未申请过该服务，请先申请该服务之后使用',
   yourApplication: '你的申请',
   unverified: '您需要完成实名认证之后方可申请该服务',
