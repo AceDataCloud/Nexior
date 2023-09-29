62c309b5d0271d7b7a910982ca310eec44b3ce8b
Germey <cqc@cuiqingcai.com>
2023-09-29 22:46:24 +0800

    update

---
+++ src/components/chat/InputBox.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="input">
+
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { ElTooltip, ElButton } from 'element-plus';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { ROUTE_CHAT_INDEX } from '@/router/constants';
+
+export default defineComponent({
+  name: 'Sidebar',
+  components: {
+    ElButton,
+    FontAwesomeIcon
+  },
+  data() {
+    return {
+      links: [
+        {
+          route: {
+            name: ROUTE_CHAT_INDEX
+          },
+          icon: 'fa-question'
+        },
+        {
+          route: {
+            name: 'midjourney-index'
+          },
+          icon: 'fa-question'
+        }
+      ]
+    };
+  }
+});
+</script>
+
+<style lang="scss" scoped>
+.sidebar {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+  .top {
+    display: flex;
+    flex-direction: column;
+  }
+  .bottom {
+    display: flex;
+    flex-direction: column;
+  }
+}
+</style>
--- src/pages/chat/Index.vue
+++ src/pages/chat/Index.vue
@@ -1,3 +1,46 @@
 <template>
-  <div>chat index</div>
+  <div class="page">
+    <div class="left">3</div>
+    <div class="right">
+      <introduction />
+      <div class="input-wrapper">
+
+      </div>
+    </div>
+  </div>
 </template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { ElMain, ElAside, ElButton, ElDrawer } from 'element-plus';
+import LeftNavigator from '@/components/common/LeftNavigator.vue';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import Introduction from '@/components/conversation/Introduction.vue';
+export default defineComponent({
+  name: 'ChatIndex',
+  components: {
+    Introduction
+  },
+  data() {
+    return {
+      drawer: false
+    };
+  }
+});
+</script>
+
+<style lang="scss">
+.page {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: row;
+  .left {
+    width: 300px;
+    height: 100%;
+  }
+  .main {
+    flex: 1;
+  }
+}
+</style>
