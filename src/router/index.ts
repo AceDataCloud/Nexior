e50892c488ce4647eb362462495a1f2902f577fc
Germey <cqc@cuiqingcai.com>
2023-09-30 12:57:09 +0800

    add paint router

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,6 +3,7 @@ import auth from './auth';
 import conversation from './conversation';
 import store from '@/store';
 import chat from './chat';
+import paint from './paint';
 import { ROUTE_AUTH_LOGIN, ROUTE_CONVERSATION_NEW, ROUTE_INDEX } from './constants';
 
 const routes = [
@@ -13,7 +14,8 @@ const routes = [
       name: ROUTE_CONVERSATION_NEW
     }
   },
-  chat
+  chat,
+  paint
 ];
 
 const router = createRouter({
