b65e3f90f6645a72773e369327153e563534f453
AceDataCloud <office@acedata.cloud>
2023-09-30 11:49:25 +0800

    add model selector

--- src/pages/chat/Index.vue
+++ src/pages/chat/Index.vue
@@ -1,11 +1,10 @@
 <template>
   <div class="page">
     <div class="left">3</div>
-    <div class="right">
+    <div class="main">
+      <model-selector v-model="model" class="model-selector" />
       <introduction />
-      <div class="input-wrapper">
-
-      </div>
+      <input-box v-model="question" class="input-box" />
     </div>
   </div>
 </template>
@@ -16,31 +15,54 @@ import { ElMain, ElAside, ElButton, ElDrawer } from 'element-plus';
 import LeftNavigator from '@/components/common/LeftNavigator.vue';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import Introduction from '@/components/conversation/Introduction.vue';
+import InputBox from '@/components/chat/InputBox.vue';
+import ModelSelector from '@/components/chat/ModelSelector.vue';
 export default defineComponent({
   name: 'ChatIndex',
   components: {
-    Introduction
+    Introduction,
+    InputBox,
+    ModelSelector
   },
   data() {
     return {
-      drawer: false
+      model: 'chatgpt',
+      drawer: false,
+      question: ''
     };
   }
 });
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
 .page {
   width: 100%;
   height: 100%;
   display: flex;
   flex-direction: row;
   .left {
+    display: none;
     width: 300px;
     height: 100%;
   }
   .main {
     flex: 1;
+    overflow: hidden;
+    position: relative;
+    .input-box {
+      display: block;
+      position: absolute;
+      bottom: 10px;
+      right: 10px;
+      left: 10px;
+    }
+    .model-selector {
+      position: absolute;
+      top: 30px;
+      left: 50%;
+      transform: translate(-50%, 0);
+      z-index: 100;
+    }
   }
 }
 </style>
