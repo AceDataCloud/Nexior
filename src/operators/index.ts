96708de3a41f1c08068dc5d59dab05613c1ac6ec
AceDataCloud <office@acedata.cloud>
2023-10-08 16:07:37 +0800

    update history

--- src/operators/index.ts
+++ src/operators/index.ts
@@ -4,3 +4,4 @@ export * from './user';
 export * from './api';
 export * from './chat';
 export * from './midjourney';
+export * from './usage';
---
+++ src/operators/usage/models.ts
@@ -0,0 +1,19 @@
+import { IApi } from '../api';
+
+export interface IApiUsage {
+  id?: string;
+  api?: IApi;
+  api_id?: string;
+  application_id?: string;
+  trace_id?: string;
+  metadata?: any;
+  created_at?: string;
+  updated_at?: string;
+}
+
+export interface IApiUsageListResponse {
+  count: number;
+  items: IApiUsage[];
+}
+
+export type IApiUsageDetailResponse = IApiUsage;
