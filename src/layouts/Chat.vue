16dae46c9f9f93602a805a720c25e57c6b802104
Germey <cqc@cuiqingcai.com>
2023-12-30 12:17:13 +0800

    update vuex

--- src/layouts/Chat.vue
+++ src/layouts/Chat.vue
@@ -14,12 +14,67 @@
 import { defineComponent } from 'vue';
 import Navigator from '@/components/common/Navigator.vue';
 import Sidebar from '@/components/chat/Sidebar.vue';
+import {
+  CHAT_MODEL_CHATGPT,
+  CHAT_MODEL_CHATGPT4,
+  CHAT_MODEL_CHATGPT4_BROWSING,
+  CHAT_MODEL_CHATGPT_16K,
+  CHAT_MODEL_CHATGPT_BROWSING,
+  applicationOperator
+} from '@/operators';
 
 export default defineComponent({
   name: 'LayoutChat',
   components: {
     Sidebar,
     Navigator
+  },
+  data() {
+    return {
+      initializing: false,
+      applications: this.$store.state.chat.applications || undefined
+    };
+  },
+  mounted() {
+    this.onFetchApplications();
+  },
+  methods: {
+    async onFetchConversations() {
+      this.loading = true;
+      const {
+        data: { items: apiUsages }
+      } = await apiUsageOperator.getAll({
+        user_id: this.$store.state.common.user.id,
+        // @ts-ignore
+        application_id: this.applications?.map((application) => application.id),
+        offset: 0,
+        limit: 30,
+        ordering: '-created_at'
+      });
+      this.loading = false;
+      // de duplicate conversations using id
+      const conversationIds: string[] = apiUsages
+        .map((apiUsage) => apiUsage.metadata?.conversation_id)
+        .filter((id) => id);
+      const uniqueConversationIds = [...new Set(conversationIds)];
+      const conversations = (await chatOperator.getConversations(uniqueConversationIds)).data;
+      this.conversations = conversations;
+    },
+    async onFetchApplications() {
+      this.initializing = true;
+      const { data: applications } = await applicationOperator.getAll({
+        user_id: this.$store.state.common.user.id,
+        api_id: [
+          CHAT_MODEL_CHATGPT.apiId,
+          CHAT_MODEL_CHATGPT_16K.apiId,
+          CHAT_MODEL_CHATGPT_BROWSING.apiId,
+          CHAT_MODEL_CHATGPT4.apiId,
+          CHAT_MODEL_CHATGPT4_BROWSING.apiId
+        ]
+      });
+      this.initializing = false;
+      this.$store.dispatch('chat/setApplications', applications?.items);
+    }
   }
 });
 </script>
