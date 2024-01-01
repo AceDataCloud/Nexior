a6b4f40342a0f14db4ccb84822aa710e536ee58b
AceDataCloud <office@acedata.cloud>
2024-01-01 16:24:56 +0800

    update

--- src/router/chat.ts
+++ src/router/chat.ts
@@ -1,14 +1,9 @@
-import { ROUTE_CHAT_CONVERSATION, ROUTE_CHAT_CONVERSATION_NEW, ROUTE_CHAT_INDEX } from './constants';
+import { ROUTE_CHAT_CONVERSATION, ROUTE_CHAT_CONVERSATION_NEW } from './constants';
 
 export default {
   path: '/chat',
   component: () => import('@/layouts/Chat.vue'),
   children: [
-    {
-      path: '',
-      name: ROUTE_CHAT_INDEX,
-      component: () => import('@/pages/chat/Index.vue')
-    },
     {
       path: 'conversation',
       name: ROUTE_CHAT_CONVERSATION_NEW,
--- src/router/constants.ts
+++ src/router/constants.ts
@@ -3,7 +3,6 @@ export const ROUTE_INDEX = 'index';
 export const ROUTE_AUTH_LOGIN = 'auth-login';
 export const ROUTE_AUTH_CALLBACK = 'auth-callback';
 
-export const ROUTE_CHAT_INDEX = 'chat-index';
 export const ROUTE_CHAT_CONVERSATION = 'chat-conversation';
 export const ROUTE_CHAT_CONVERSATION_NEW = 'chat-conversation-new';
 
