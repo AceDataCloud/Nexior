40817fff93bb5dfc95c436e468c0bf1b8ad4b98d
Germey <cqc@cuiqingcai.com>
2024-01-10 03:02:06 +0800

    migrate

--- src/pages/midjourney/History.vue
+++ src/pages/midjourney/History.vue
@@ -35,7 +35,7 @@ interface IData {
   ignore: string;
 }
 
-const CALLBACK_URL = 'https://webhook.zhishuyun.com/midjourney';
+const CALLBACK_URL = 'https://webhook.acedata.cloud/midjourney';
 
 export default defineComponent({
   name: 'MidjourneyIndex',
