606de9c796491f3dc8974b9c2189b52e2a88a3f8
Germey <cqc@cuiqingcai.com>
2024-04-22 13:00:57 +0800

    update trans

--- deploy/production/scripts/deploy.sh
+++ deploy/production/scripts/deploy.sh
@@ -1,3 +1,2 @@
 cat deploy/production/deployment.yaml | sed 's/\${TAG}/'"$BUILD_NUMBER"'/g' | kubectl apply -f - || true
 kubectl apply -f deploy/production/service.yaml
-kubectl apply -f deploy/production/ingress.yaml
