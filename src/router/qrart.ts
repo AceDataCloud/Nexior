b46f57d3bfe0a3591cac5862b308be198c41e338
Germey <germey@acedata.cloud>
2024-04-28 21:26:22 +0800

    add basic qrart

--- src/router/qrart.ts
+++ src/router/qrart.ts
@@ -1,4 +1,4 @@
-import { ROUTE_QRART_HISTORY, ROUTE_QRART_INDEX } from './constants';
+import { ROUTE_QRART_INDEX } from './constants';
 
 export default {
   path: '/qrart',
@@ -11,11 +11,6 @@ export default {
       path: '',
       name: ROUTE_QRART_INDEX,
       component: () => import('@/pages/qrart/Index.vue')
-    },
-    {
-      path: 'history',
-      name: ROUTE_QRART_HISTORY,
-      component: () => import('@/pages/qrart/History.vue')
     }
   ]
 };
