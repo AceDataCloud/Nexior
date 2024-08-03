d820a5c641006cd23a5cc86a01e062e93b0b6378
AceDataCloud <office@acedata.cloud>
2024-08-04 00:27:28 +0800

    update test env

--- src/constants/endpoint.ts
+++ src/constants/endpoint.ts
@@ -1,5 +1,6 @@
-export const BASE_URL_PLATFORM = 'https://platform.acedata.cloud';
-export const BASE_URL_HUB = 'https://hub.acedata.cloud';
-export const BASE_URL_AUTH = 'https://auth.acedata.cloud';
-export const BASE_URL_API = 'https://api.acedata.cloud';
-export const BASE_URL_API2 = 'https://api2.acedata.cloud';
+const isTest = window.location.origin === 'https://hub.acedata.cloud';
+
+export const BASE_URL_PLATFORM = isTest ? 'https://platform-test.acedata.cloud' : 'https://platform.acedata.cloud';
+export const BASE_URL_HUB = isTest ? 'https://hub-test.acedata.cloud' : 'https://hub.acedata.cloud';
+export const BASE_URL_AUTH = isTest ? 'https://auth-test.acedata.cloud' : 'https://auth.acedata.cloud';
+export const BASE_URL_API = isTest ? 'https://api-test.acedata.cloud' : 'https://api.acedata.cloud';
