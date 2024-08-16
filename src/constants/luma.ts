2e55c32c9918eb8e4ec661e86bcae5a7f6f1ae23
hyf-github-user <1348977728@qq.com>
2024-08-16 17:16:40 +0800

    fix luma redundancy

--- src/constants/luma.ts
+++ src/constants/luma.ts
@@ -2,16 +2,3 @@ export const LUMA_SERVICE_ID = 'dff45a0d-2858-4936-8e2b-3d49c52aff11';
 
 export const LUMA_DEFAULT_ENHANCEMENT = false;
 export const LUMA_DEFAULT_LOOP = false;
-export const LUMA_DEFAULT_PRESET = '';
-export const LUMA_DEFAULT_TYPE = 'text';
-export const LUMA_DEFAULT_ASPECT_RATIO = '1:1';
-export const LUMA_DEFAULT_ADVANCED = false;
-export const LUMA_DEFAULT_RAWURL = true;
-export const LUMA_DEFAULT_PADDING_LEVEL = 5;
-export const LUMA_DEFAULT_POSITION = 'center';
-export const LUMA_DEFAULT_PIXEL_STYLE = 'square';
-export const LUMA_DEFAULT_MARKER_SHAPE = 'square';
-export const LUMA_DEFAULT_SUB_MARKER = 'square';
-export const LUMA_DEFAULT_ROTATE = 0;
-export const LUMA_DEFAULT_ECL = 'H';
-export const LUMA_DEFAULT_PADDING_NOISE = 0;
