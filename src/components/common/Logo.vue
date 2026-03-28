eed5ac5d3da35224e54f3b1032393d5478986dbf
Ace Data Cloud Dev <dev@acedata.cloud>
2026-03-29 01:29:41 +0800

    Refine mobile download landing page

--- src/components/common/Logo.vue
+++ src/components/common/Logo.vue
@@ -1,20 +1,42 @@
 <template>
-  <img :src="url" class="logo" @click="$emit('click')" />
+  <button type="button" class="brand-logo" :aria-label="siteTitle" @click="$emit('click')">
+    <img :src="url" class="brand-logo__image" :alt="siteTitle" />
+  </button>
 </template>
 
 <script lang="ts">
-import { getCookie } from 'typescript-cookie';
 import { defineComponent } from 'vue';
 
 export default defineComponent({
   emits: ['click'],
   computed: {
-    dark() {
-      return getCookie('THEME') === 'dark';
+    siteTitle() {
+      return this.$store.state.site?.title || 'AceData';
     },
     url() {
-      return this.$store.state.site?.favicon;
+      return this.$store.state.site?.logo || this.$store.state.site?.favicon;
     }
   }
 });
 </script>
+
+<style lang="scss" scoped>
+.brand-logo {
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  padding: 0;
+  border: 0;
+  background: transparent;
+  cursor: pointer;
+
+  &__image {
+    display: block;
+    width: auto;
+    max-width: 180px;
+    height: 42px;
+    object-fit: contain;
+    object-position: left center;
+  }
+}
+</style>
