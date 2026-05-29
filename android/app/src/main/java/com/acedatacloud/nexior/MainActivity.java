e550c8b792a339dc45307cdf6bfc0ac2a7ad8b18
Ace Data Cloud Dev <dev@acedata.cloud>
2026-05-29 23:10:49 +0800

    fix(android): enable edge-to-edge and replace deprecated cutout mode (#820)

--- android/app/src/main/java/com/acedatacloud/nexior/MainActivity.java
+++ android/app/src/main/java/com/acedatacloud/nexior/MainActivity.java
@@ -1,9 +1,17 @@
 package com.acedatacloud.nexior;
 
+import android.os.Bundle;
+import androidx.activity.EdgeToEdge;
 import com.getcapacitor.BridgeActivity;
 
 /**
  * MainActivity is the main entry point for the Nexior Android application.
  * It extends BridgeActivity to provide Capacitor functionality.
  */
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        EdgeToEdge.enable(this);
+        super.onCreate(savedInstanceState);
+    }
+}
