891b1f1e5fd3dd5c5bd7e735157ed59a24af4247
Ace Data Cloud Dev <dev@acedata.cloud>
2026-05-30 23:31:26 +0800

    feat(public-pay): anonymous order pay page at /orders/:id (#836)

---
+++ src/components/order/public/AliPay.vue
@@ -0,0 +1,47 @@
+<template>
+  <el-dialog
+    :model-value="visible"
+    :title="$t('application.title.buyService')"
+    width="500px"
+    center
+    @close="$emit('hide')"
+  >
+    <p class="text-center">
+      {{ $t('order.message.buyInExternalUrl') }}
+    </p>
+  </el-dialog>
+</template>
+
+<script lang="ts">
+import { IOrder } from '@/models';
+import { defineComponent } from 'vue';
+import { ElDialog } from 'element-plus';
+
+export default defineComponent({
+  name: 'PublicAlipayPayDialog',
+  components: {
+    ElDialog
+  },
+  props: {
+    order: {
+      type: Object as () => IOrder,
+      required: true
+    },
+    visible: {
+      type: Boolean,
+      required: false,
+      default: false
+    }
+  },
+  emits: ['hide'],
+  watch: {
+    visible: {
+      handler(val) {
+        if (val && this.order?.pay_url) {
+          window.open(this.order.pay_url, '_blank');
+        }
+      }
+    }
+  }
+});
+</script>
