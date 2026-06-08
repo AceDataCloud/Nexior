b43df72188b0096a4eeecc0595088830f20ec27f
Ace Data Cloud Dev <dev@acedata.cloud>
2026-06-09 02:05:35 +0800

    fix(download): remove top navigation header from the download page (#875)

---
+++ src/layouts/Bare.vue
@@ -0,0 +1,24 @@
+<template>
+  <el-container class="wrapper min-h-[100vh]">
+    <el-main class="main"> <router-view /> </el-main>
+    <el-footer class="footer p-0 h-auto">
+      <bottom-footer />
+    </el-footer>
+  </el-container>
+</template>
+
+<script lang="ts">
+import BottomFooter from '@/components/common/BottomFooter.vue';
+import { ElContainer, ElMain, ElFooter } from 'element-plus';
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'LayoutBare',
+  components: {
+    BottomFooter,
+    ElContainer,
+    ElMain,
+    ElFooter
+  }
+});
+</script>
