0d2193ec27b22b17be0f140b71d12752451eb08e
Ace Data Cloud Dev <dev@acedata.cloud>
2026-06-28 02:04:05 +0800

    feat(download): enable iOS App Store entry on studio download page (#1055)

--- src/constants/mobile.ts
+++ src/constants/mobile.ts
@@ -4,6 +4,8 @@ export const MOBILE_ANDROID_DOWNLOAD_URL = 'https://cdn.acedata.cloud/2f29543715
 
 export const MOBILE_ANDROID_PLAY_STORE_URL = 'https://play.google.com/store/apps/details?id=com.acedatacloud.nexior';
 
+export const MOBILE_IOS_APP_STORE_URL = 'https://apps.apple.com/app/id6772432921';
+
 export const MOBILE_IOS_DOWNLOAD_URL = '';
 
 export const MOBILE_IOS_FALLBACK_URL = '';
